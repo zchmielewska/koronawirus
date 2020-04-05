fd80b1baa31e1a31eafc9b5293e94db002fdba74
Zuzanna <zchmielewska@gmail.com>
2020-04-05 18:52:54 +0200

    use rdrop

--- README.md
+++
@@ -1,5 +0,0 @@
-# koronawirus
-
-Dane dotyczące koronawirusa w Polsce na podstawie publikacja ECDC.
-
-Prezentacja w postaci aplikacji R Shiny: [zchmielewska.shinyapps.io/koronawirus](https://zchmielewska.shinyapps.io/koronawirus/)
--- utils/utility-functions.R
+++ utils/utility-functions.R
@@ -10,11 +10,35 @@ aggregate <- function(v) {
   return(result)
 }
 
-loadECDC <- function() {
-  ecdc <- list()
-  data.raw <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", stringsAsFactors = FALSE)
+loadLastFile <- function() {
+  last.file <- drop_dir("ECDC") %>% select(name) %>% arrange(desc(name)) %>% slice(1) %>% pull()
+  last.data <- drop_read_csv(paste0("ECDC/", last.file))
+  return(last.data)
+}
+
+checkNewData <- function(data) {
+  result <- data
   
-  ecdc$data <- data.raw %>% 
+  # if the data is from yesterday; check if new exists
+  last.file <- drop_dir("ECDC") %>% select(name) %>% arrange(desc(name)) %>% slice(1) %>% pull()
+  if(as.Date(gsub(".csv", "", last.file)) < Sys.Date()) {
+    base.url <- "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-"
+    new.exists <- !(testit::has_error(rio::import(paste0(base.url, Sys.Date(), ".xlsx"))))
+    if(new.exists) {
+      ecdc.data <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", stringsAsFactors = FALSE)
+      ecdc.filename <- paste0(Sys.Date(), ".csv")
+      write.csv(ecdc.data, ecdc.filename)
+      drop_upload(ecdc.filename, path = "ECDC")
+      last.data <- drop_read_csv(paste0("ECDC/", ecdc.filename))
+      result <- last.data
+    }
+  }
+  
+  return(result)
+}
+
+prepareECDCdata <- function(data) {
+  result <- data %>% 
     as_tibble() %>% 
     mutate(Date = as.Date(paste(year, month, day, sep="-"))) %>% 
     rename(
@@ -24,9 +48,7 @@ loadECDC <- function() {
     mutate(Date = as.Date(Date),
            DeathsDelta = as.integer(DeathsDelta))
   
-  ecdc$date <- max(ecdc$data$Date)
-  
-  return(ecdc)    
+  return(result)    
 }
 
 getPolandData <- function(data) {
